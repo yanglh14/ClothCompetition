bf920479873014e8e13c27bb74ff93b65efc1707
yanglh <yanglh@connect.hku.hk>
2024-05-08 17:17:47 +0800

    [A] sample a grasp position from the point cloud

--- ClothCompetition/real_robot/scripts/grasp.py
+++ ClothCompetition/real_robot/scripts/grasp.py
@@ -6,6 +6,7 @@
 import cv2
 import time
 import numpy as np
+from random import sample
 import rospy
 import os
 import yaml
@@ -25,6 +26,33 @@ class Grasp:
             time.sleep(0.1)
         return self.rs_listener.image.copy()
 
+    def sample_grasp_posi(self, pc):
+        # set region of interest (roi)
+        x = pc[:, 0]
+        y = pc[:, 1]
+        z = pc[:, 2]
+        z_min, z_max = np.min(z), np.max(z)
+        z_interval = z_max - z_min
+        pool_part = [1,2,2,3,3,3,4,4,4,4]
+        # sample a point from pool_part
+        target_part = sample(pool_part, 1)[0]
+        # determine the region of target part
+        z_roi_min = z_min + z_interval * (target_part - 1) / 4
+        z_roi_max = z_min + z_interval * target_part / 4
+        print(f'z_roi: [{z_roi_min, z_roi_max}]')
+        # sample a point with z in [z_roi_min, z_roi_max]
+        idx_arr = np.where((z >= z_roi_min) & (z <= z_roi_max))[0]
+        if idx_arr is not None:
+            # sample a point from idx_ls
+            idx = np.random.choice(idx_arr)
+        else:
+            # throw a warning
+            raise ValueError('No point in the region of interest')
+        # find out the corresponding pc
+        grasp_pose = pc[idx]
+        return grasp_pose
+
+
 
 if __name__ == '__main__':
     gp = Grasp()
@@ -35,6 +63,13 @@ if __name__ == '__main__':
     # cv2.imwrite('../log/mask_comp.png', mask)
     ## get the vox_pc of the mask
     cloth_pc = gp.rs_listener.get_pc_given_mask(mask)
+    print('cloth_pc:', cloth_pc.shape)
+
+    # sample a grasp position from the point cloud
+    grasp_pose = gp.sample_grasp_posi(cloth_pc)
+
+    # move R arm to the grasp pose (with initial orientation)
+    gp.env.move(grasp_pose, arm='right')
 
 
 
