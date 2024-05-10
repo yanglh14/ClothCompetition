cd2b590075aa3a4ff5b3dd5c223b465ae5fdd461
yanglh <yanglh@connect.hku.hk>
2024-05-10 19:05:08 +0800

    [A] get edges from rbg image

--- ClothCompetition/real_robot/scripts/grasp_RHLP_via_pc.py
+++ ClothCompetition/real_robot/scripts/grasp_RHLP_via_pc.py
@@ -145,10 +145,40 @@ class Grasp:
         action_L = np.array([X_L_ls, Y_L_ls, Z_L_ls]).T
         action_R = np.array([X_R_ls, Y_R_ls, Z_R_ls]).T
         return action_L, action_R, dt
+    def get_edge(self):
+        image = self.get_image()
+        # Convert to graycsale
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Blur the image for better edge detection
+        img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
 
+        # Sobel Edge Detection
+        sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)  # Sobel Edge Detection on the X axis
+        sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)  # Sobel Edge Detection on the Y axis
+        sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1,
+                            ksize=5)  # Combined X and Y Sobel Edge Detection
+        # # Display Sobel Edge Detection Images
+        # cv2.imshow('Sobel X', sobelx)
+        # cv2.waitKey(0)
+        # cv2.imshow('Sobel Y', sobely)
+        # cv2.waitKey(0)
+        # cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
+        # cv2.waitKey(0)
+
+        # Canny Edge Detection
+        edges = cv2.Canny(image=img_blur, threshold1=1, threshold2=200)  # Canny Edge Detection
+        # save Canny Edge Detection Image
+        cv2.imwrite('../log/edges.png', edges)
+        # cv2.waitKey(0)
+
+        cv2.destroyAllWindows()
+        return edges
 if __name__ == '__main__':
     gp = Grasp()
-    gp.env.gripper_open()
+    # gp.get_edge()
+    # gp.env.gripper_open()
+    gp.env.robot_left.set_gripper_open(True)
+
     gp.env.reset()
     gp.env.gripper_close()
     gp.env.robot_left.set_gripper_open(True)
